9c403554cf672181a293a6c51ce530ffee641e16
Kirill Biryukov <KBiryukov@t1-team.ru>
2026-07-01 11:57:15 +0300

    asdasdas2

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println(111);
+        System.out.println(222);
     }
 }
